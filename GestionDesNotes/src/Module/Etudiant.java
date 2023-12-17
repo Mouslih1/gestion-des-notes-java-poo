f9c92f1b56e84d97206497c401e938f4476ed250
Mouslih Marouane <106397107+Mouslih0@users.noreply.github.com>
2023-12-17 18:55:37 +0100

    Add files via upload

---
+++ GestionDesNotes/src/Module/Etudiant.java
@@ -0,0 +1,27 @@
+package Module;
+
+import java.util.ArrayList;
+
+public class Etudiant {
+    private String name;
+
+    public Etudiant(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Etudiant{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
